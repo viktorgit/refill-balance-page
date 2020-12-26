39d25e03c919bb81024dc65b0d1cec7209164be7
kabanovvyu <kabanovvyu@1forma.ru>
2020-12-27 00:40:44 +0300

    add mask

--- src/app/pages/refill-balance/refill-balance.module.ts
+++ src/app/pages/refill-balance/refill-balance.module.ts
@@ -5,12 +5,16 @@ import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDialogModule } from '@angular/material/dialog';
+import { NgxMaskModule, IConfig } from 'ngx-mask'
 
 import { RefillBalanceRoutingModule } from './refill-balance-routing.module';
 import { RefillBalanceComponent } from './refill-balance.component';
 import { RefillBalanceService } from './refill-balance.service';
 import { SuccesOrErrorDialog } from './success-or-error-dialog/success-or-error-dialog.component';
 
+const maskConfig: Partial<IConfig> = {
+  validation: false,
+};
 
 @NgModule({
   declarations: [RefillBalanceComponent, SuccesOrErrorDialog],
@@ -22,7 +26,8 @@ import { SuccesOrErrorDialog } from './success-or-error-dialog/success-or-error-
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
-    MatDialogModule
+    MatDialogModule,
+    NgxMaskModule.forRoot(maskConfig)
   ],
   entryComponents: [SuccesOrErrorDialog],
   providers: [RefillBalanceService]
